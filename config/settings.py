fe6c605b67efc5dcc2347041376f1ce664e62a61
Délita Makanda <delita.makanda@gmail.com>
2022-08-14 03:13:43 +0200

    add packages

--- config/settings.py
+++ config/settings.py
@@ -119,8 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- config/settings_production.py
+++ config/settings_production.py
@@ -1,6 +1,10 @@
 from config.settings import *
 import os
 
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+DEBUG = os.environ['DEBUG']
+
 hostname = os.environ['DBHOST']
 
 # Configure Postgres database; the full username for PostgreSQL flexible server is
@@ -14,3 +18,27 @@ DATABASES = {
         'PASSWORD': os.environ['DBPASS'] 
     }
 }
+
+# WhiteNoise configuration
+MIDDLEWARE += [                                                                                       
+    'whitenoise.middleware.WhiteNoiseMiddleware',                 
+]
+
+STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
+
+# email 
+DEFAULT_FROM_EMAIL = os.environ['ADMIN_EMAIL']
+SERVER_EMAIL = os.environ['ADMIN_EMAIL']
+
+ADMINS = [
+  (os.environ['ADMIN_NAME'], os.environ['ADMIN_EMAIL']),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['SENDGRID_SERVER']
+EMAIL_PORT = os.environ['SENDGRID_PORT']
+EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
+EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_TIMEOUT = 500
